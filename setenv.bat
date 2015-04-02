a637620bfcad70daa25ba40b5dab1b683cc57761
Vogel, Benno <Benno.Vogel@zuehlke.com>
2015-04-02 15:57:55 +0200

    Fix JBOSS_HOME

--- setenv.bat
+++ setenv.bat
@@ -5,14 +5,14 @@ rem ================================================
 call %~dp0conf\devpack.bat
 set CONF_DIR=%WORK_DRIVE%:\conf
 set WORKING_DIR=%WORK_DRIVE%:\
-set TOOLS_DIR=%WORKING_DIR%\tools
-set BIN_DIR=%WORKING_DIR%\bin
-set M2_HOME=%WORKING_DIR%\tools\mvn
+set TOOLS_DIR=%WORKING_DIR%tools
+set BIN_DIR=%WORKING_DIR%bin
+set M2_HOME=%WORKING_DIR%tools\mvn
 
 set M2_HOME=%TOOLS_DIR%\mvn
 set FORGE_HOME=%TOOLS_DIR%\forge-distribution-1.0.0.Final
 set JAVA_HOME=%TOOLS_DIR%\jdk_8
-set JBOSS_HOME=%TOOLS_DIR%\wildfly-8.2.0.Final
+set JBOSS_HOME=%TOOLS_DIR%\wildfly
 
 rem -----------------------------------------------------------------
 rem Add workspace parameters as required
