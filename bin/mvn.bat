aa2e8887e8a667e0c9040e1c736df14a76fdb1f5
Vogel, Benno <Benno.Vogel@zuehlke.com>
2015-04-10 10:23:43 +0200

    Added public / private maven config

---
+++ bin/mvn.bat
@@ -0,0 +1,20 @@
+@echo off
+rem =====================================
+rem Maven Wrapper for JEE Devpack
+rem - Use maven from the dev pack
+rem - Use maven setting from the dev pack
+rem =====================================
+
+set M2_SETTINGS=-gs %M2_HOME%\conf\mvn-public-settings.xml -s %PRIVATE_M2_CONFIG%
+
+rem Initialise command line...
+set MAVEN_CMD_LINE_ARGS=%M2_SETTINGS%
+:startInit
+if %1a==a goto endInit
+set MAVEN_CMD_LINE_ARGS=%MAVEN_CMD_LINE_ARGS% %1
+shift
+goto startInit
+:endInit
+
+rem call maven
+%M2_HOME%\bin\mvn.bat %MAVEN_CMD_LINE_ARGS%
--- conf/devpack.bat
+++ conf/devpack.bat
@@ -9,6 +9,9 @@ set WORK_DRIVE=W
 rem You may customize your eclipse workspace location here
 set WORKSPACE=%WORK_DRIVE%:\workspace
 
+rem Private maven settings (e.g. server passwords)
+set PRIVATE_M2_CONFIG=%WORK_DRIVE%:\conf\mvn-private-settings.xml
+
 rem Configure your favourite editor here
 rem set EDITOR=%WORK_DRIVE%:\tools\atom\atom.exe
 set EDITOR=%WORK_DRIVE%:\tools\npp\notepad++.exe
