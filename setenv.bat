fccb3db7148af09de25ac414d150d5a9a014962d
Benno Vogel <Benno.Vogel@zuehlke.com>
2017-02-14 11:21:10 +0100

    Add command aliases

--- setenv.bat
+++ setenv.bat
@@ -17,3 +17,13 @@ rem -----------------------------------------------------------------
 rem Add workspace parameters as required
 
 set PATH=%BIN_DIR%;%JAVA_HOME%\bin;%FORGE_HOME%\bin;%TOOLS_DIR%\scala\bin;%TOOLS_DIR%\tomee\bin;%PATH%
+
+rem -----------------------------------------------------------------
+rem Define command aliases
+doskey mci=mvn clean install
+doskey mcp=mvn clean package
+doskey st=sourcetree
+
+rem Add more aliases for your convenience...
+rem doskey ..=cd ..
+rem doskey ...=cd ..\..
