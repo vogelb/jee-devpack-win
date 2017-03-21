c11a7a8b0943594dddfba8c3739f201aabdb6e22
Benno Vogel <mail@benno-vogel.de>
2017-03-21 23:53:07 +0100

    Fix PATH, console start

--- setenv.bat
+++ setenv.bat
@@ -12,9 +12,6 @@ set M2_HOME=%TOOLS_DIR%\mvn
 set JAVA_HOME=%TOOLS_DIR%\jdk_8
 
 rem -----------------------------------------------------------------
-rem Add workspace parameters as required
-set PATH=%BIN_DIR%;%JAVA_HOME%\bin;%WORKING_DIR%;%PATH%
-
 rem load optional package configuration
 call :include_config %CONF_DIR%\postgres.bat
 call :include_config %CONF_DIR%\jboss.bat
@@ -23,6 +20,9 @@ call :include_config %CONF_DIR%\forge.bat
 call :include_config %CONF_DIR%\scala.bat
 call :include_config %CONF_DIR%\git.bat
 
+rem -----------------------------------------------------------------
+rem Add workspace parameters as required
+set PATH=%BIN_DIR%;%JAVA_HOME%\bin;%WORKING_DIR%;%PATH%
 
 rem -----------------------------------------------------------------
 rem Define command aliases
--- start_command_shell.bat
+++ start_command_shell.bat
@@ -1,10 +1,14 @@
 @echo off
 rem ===================================================================
 rem Console Start Script
-rem Please configure the desired eclipse version and workspace location
 rem ===================================================================
 call bin\mount_devpack.bat
 cd /d %WORK_DRIVE%:\
 call setenv.bat
-console -w "Dev Console" -d "%WORKSPACE%" -r "/K call %WORK_DRIVE%:\setenv.bat" -c conf\console.xml
 
+if exist %WORKSPACE% (
+	set START_DIR="%WORKSPACE%"
+) else (
+	set START_DIR="%WORK_DRIVE%:"\
+)
+console -w "Dev Console" -d %START_DIR%" -r "/K call %WORK_DRIVE%:\setenv.bat" -c conf\console.xml
