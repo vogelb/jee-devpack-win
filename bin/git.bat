0d25838810b198e3601d2b64b5ab4cfc9115f059
Benno Vogel <Benno.Vogel@zuehlke.com>
2017-03-21 13:36:01 +0100

    Fix git wrapper

--- bin/git.bat
+++ bin/git.bat
@@ -1,18 +1,13 @@
-@echo off && setlocal
+@echo off
 rem ===================================================================
 rem DevPack git wrappper
 rem Displays git branch in the command prompt.
 rem ===================================================================
 git.exe %*
 if "%DEVPACK_GIT_PROMPT%" == "TRUE" (
-  set GITBRANCH=
-  for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
-
-  if "%GITBRANCH%" == "" (
-    prompt $P$G 
-  ) else (
-    prompt $P $C$E[1;7;32;47m%GITBRANCH%$E[0m$F $G 
+  for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do (
+	prompt $P $C$E[1;7;32;47m%%I$E[0m$F $G
+	exit /B
   )
-) else (
-  prompt $P$G 
 )
+prompt $P$G
