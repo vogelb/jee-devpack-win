f848b2a2307dbc843266825b6baa00a0c6db1150
Benno Vogel <Benno.Vogel@zuehlke.com>
2017-03-21 12:05:25 +0100

    Remove duplicate setting

--- setup.bat
+++ setup.bat
@@ -19,8 +19,6 @@ rem Setup configuration
 rem Set DOWNLOADS_DIR in order to reuse existing downloads  
 set DOWNLOADS_DIR=%~dp0downloads
 
-set TEMPLATE_DIR=%~dp0templates
-
 rem KEEP_PACKAGES: If set to true, downloaded packages will not be deleted after installation
 set KEEP_PACKAGES=TRUE
 
