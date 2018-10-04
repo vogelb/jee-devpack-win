81b091e44fbc0c646bb25e3bfe2d28d635e158bf
Benno Vogel <benno.vogel@gebit.de>
2018-10-04 16:13:33 +0200

    Fix default devpack name

--- conf/devpack.bat
+++ conf/devpack.bat
@@ -13,6 +13,7 @@ if "%DEVPACK_BASE%" == "" (
 rem Set the working drive letter here. The default is W.
 set WORK_DRIVE=W
 set DEVPACK_NAME=POS-WS
+set DEVPACK_NAME=Java Dev Pack
 
 rem Set the default JDK
 set DEVPACK_DEFAULT_JDK=JDK10
